a8b6a0b9933c0f07f32be29e4235dc355f0bc832
Kenichi Utsumi <k-utsumi@nttpc.co.jp>
2017-06-05 07:37:14 +0000

    until_DIVE03

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,57 @@
+class TweetsController < ApplicationController
+    before_action :set_tweet, only: [:edit, :update, :destroy]
+
+  def index
+     @tweets=Tweet.all
+  end
+  
+  def new
+    if params[:back]
+      @tweet = Tweet.new(tweets_params)
+    else
+      @tweet = Tweet.new
+    end
+  end
+
+  def create
+    @tweet=Tweet.create(tweets_params)
+    if @tweet.save
+    redirect_to tweets_path, notice: "Tweetを作成しました！"
+    else
+    render 'new'
+    end
+  end
+  
+  def confirm
+    @tweet=Tweet.new(tweets_params)
+    render :new if @tweet.invalid?
+  end
+  
+  def edit
+        # edit, update, destroyで共通コード
+  end
+  
+  def update
+        # edit, update, destroyで共通コード
+    if @tweet.update(tweets_params)
+      redirect_to tweets_path, notice: "Tweetを更新しました！"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+        # edit, update, destroyで共通コード
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweetを削除しました！"
+  end
+  
+    private
+    def tweets_params
+      params.require(:tweet).permit(:content)
+    end
+    
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
+    end
+end
