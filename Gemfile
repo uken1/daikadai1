47d9ef26f3ae560f38c2ef3e22c3239191d072e4
Kenichi Utsumi <k-utsumi@nttpc.co.jp>
2017-06-07 06:30:43 +0000

    2st commit

--- Gemfile
+++ Gemfile
@@ -43,3 +43,6 @@ group :development, :test do
   gem 'spring'
 end
 
+gem 'rails_12factor', group: :production
+gem 'twitter-bootstrap-rails'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   resources :tweets, only: [:index, :new, :create, :edit, :update ,:destroy] do
     collection do
       post :confirm
     end
   end
+  
+    root 'top#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
